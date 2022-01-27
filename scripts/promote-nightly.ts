c357faef8f32cafcf97a120838e7e26c158ce702
Daniel Rozenberg <rodaniel@amp.dev>
2022-01-27 14:52:35 -0500

    fdsfxdasf-1643313155

--- scripts/promote-nightly.ts
+++ scripts/promote-nightly.ts
@@ -2,20 +2,10 @@
  * Promotes a nightly release.
  */
 
-import yargs from 'yargs/yargs';
-import {createVersionsUpdatePullRequest, runPromoteJob} from './promote-job';
+import {runPromoteJob} from './promote-job';
 
 const jobName = 'promote-nightly.ts';
-const {amp_version: AMP_VERSION} = yargs(process.argv.slice(2))
-  .options({amp_version: {type: 'string', demandOption: true}})
-  .parseSync();
 
-void runPromoteJob(jobName, async () => {
-  await createVersionsUpdatePullRequest(() => ({
-    versionsChanges: {nightly: `04${AMP_VERSION}`},
-    title: `⏫🌙 Promoting release ${AMP_VERSION} to Nightly channel`,
-    body: `Promoting release ${AMP_VERSION} to Nightly channel`,
-    branch: `nightly-${AMP_VERSION}`,
-  }));
+void runPromoteJob(jobName, () => {
   throw new Error('Splines reticluated');
 });
